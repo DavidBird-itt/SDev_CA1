24a3c5cd3105bedad4a470390ed56ef787ae663a
x00148542 <x00148542@outlook.com>
2019-03-11 11:58:19 +0000

    Fixed some things for the database.html

--- target/scala-2.12/twirl/main/views/html/databaseTest.template.scala
+++ target/scala-2.12/twirl/main/views/html/databaseTest.template.scala
@@ -33,19 +33,31 @@ object databaseTest extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.a
 Seq[Any](_display_(/*2.2*/main("Database", emps)/*2.24*/ {_display_(Seq[Any](format.raw/*2.26*/("""
 """),format.raw/*3.1*/("""<p>It works!</p>
 
+<table class="table table-bordered table-hover table-condensed">
+        <thead>
+            <!--Header-->
+            <tr>
+                <th>ID</th>
+                <th>Type</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
 <tbody>
     <!-- Loop for creating table rows from Employee data  -->
 
+    
     <tr>
-        """),_display_(/*9.10*/for(i<-elist) yield /*9.23*/ {_display_(Seq[Any](format.raw/*9.25*/("""
-             """),format.raw/*10.14*/("""<td>"""),_display_(/*10.19*/i/*10.20*/.getId),format.raw/*10.26*/("""</td>
-            <td>"""),_display_(/*11.18*/i/*11.19*/.getType),format.raw/*11.27*/("""</td>
-            <td>"""),_display_(/*12.18*/i/*12.19*/.getfName),format.raw/*12.28*/("""</td>
-            <td>"""),_display_(/*13.18*/i/*13.19*/.getlName),format.raw/*13.28*/("""</td>
-            <td>"""),_display_(/*14.18*/i/*14.19*/.getSalary),format.raw/*14.29*/("""</td>
+        """),_display_(/*21.10*/for(i<-elist) yield /*21.23*/ {_display_(Seq[Any](format.raw/*21.25*/("""
+             """),format.raw/*22.14*/("""<td>"""),_display_(/*22.19*/i/*22.20*/.getId),format.raw/*22.26*/("""</td>
+            <td>"""),_display_(/*23.18*/i/*23.19*/.getType),format.raw/*23.27*/("""</td>
+            <td>"""),_display_(/*24.18*/i/*24.19*/.getfName),format.raw/*24.28*/("""</td>
+            <td>"""),_display_(/*25.18*/i/*25.19*/.getlName),format.raw/*25.28*/("""</td>
+            <td>&euro; """),_display_(/*26.25*/("%.2f".format(i.getSalary))),format.raw/*26.53*/("""</td>
 
-            """)))}),format.raw/*16.14*/(""" 
-        """),format.raw/*17.9*/("""</tr>
+        """)))}),format.raw/*28.10*/(""" 
+        """),format.raw/*29.9*/("""</tr>
 
 </tbody>
 """)))}))
@@ -64,11 +76,11 @@ Seq[Any](_display_(/*2.2*/main("Database", emps)/*2.24*/ {_display_(Seq[Any](for
 
               /*
                   -- GENERATED --
-                  DATE: Mon Mar 11 11:22:55 GMT 2019
+                  DATE: Mon Mar 11 11:55:28 GMT 2019
                   SOURCE: /home/wdd/SDevProj/SDev_CA1/app/views/databaseTest.scala.html
-                  HASH: 5fb3660124ab3311338ce3a0b0622053c5f23622
-                  MATRIX: 1000->1|1161->70|1191->92|1230->94|1257->95|1390->202|1418->215|1457->217|1499->231|1531->236|1541->237|1568->243|1618->266|1628->267|1657->275|1707->298|1717->299|1747->308|1797->331|1807->332|1837->341|1887->364|1897->365|1928->375|1979->395|2016->405
-                  LINES: 28->1|33->2|33->2|33->2|34->3|40->9|40->9|40->9|41->10|41->10|41->10|41->10|42->11|42->11|42->11|43->12|43->12|43->12|44->13|44->13|44->13|45->14|45->14|45->14|47->16|48->17
+                  HASH: 3496ddd99d57ed453c29f513ebcae6254c1261cf
+                  MATRIX: 1000->1|1161->70|1191->92|1230->94|1257->95|1716->527|1745->540|1785->542|1827->556|1859->561|1869->562|1896->568|1946->591|1956->592|1985->600|2035->623|2045->624|2075->633|2125->656|2135->657|2165->666|2222->696|2271->724|2318->740|2355->750
+                  LINES: 28->1|33->2|33->2|33->2|34->3|52->21|52->21|52->21|53->22|53->22|53->22|53->22|54->23|54->23|54->23|55->24|55->24|55->24|56->25|56->25|56->25|57->26|57->26|59->28|60->29
                   -- GENERATED --
               */
           
