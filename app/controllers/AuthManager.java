c2e8bdba0e63df0d0febb2622131e1f1c85e5658
Karl Blair <kblr11111@gmail.com>
2019-03-13 11:22:10 +0000

    more manager security and priv. not finished

---
+++ app/controllers/AuthManager.java
@@ -0,0 +1,25 @@
+// package controllers;
+
+// import play.mvc.*;
+// import java.util.concurrent.CompletionStage;
+// import java.util.concurrent.CompletableFuture;
+
+// import models.users.*;
+
+// public class AuthManager extends Action.Simple {
+//     public CompletionStage<Result> call(Http.Context ctx) {
+
+//         String id = ctx.session().get("empId");
+
+//         if (id != null) {
+//             Employees e = Employees.getEmployeeById(id);
+//             if ("Manager".equals(e.getRole())) {
+//                 return delegate.call(ctx);
+//             }
+//         }
+
+//         ctx.flash().put("error", "Manager must be logged in.");
+
+//         return CompletableFuture.CompletableFuture(redirect(routes.LoginController.login()));
+//     }
+// }
